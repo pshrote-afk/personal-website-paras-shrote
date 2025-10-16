105607fe56f36b08eb426750d81c612d15a78318
pshrote-afk <pshrote1@gmail.com>
2025-10-16 12:24:48 +0530

    #12 hide all other nebula buttons when any nebula button is clicked

--- assets/js/toggleAuthor.js
+++ assets/js/toggleAuthor.js
@@ -2,6 +2,7 @@ const toggle = document.getElementById('toggleAuthor');
 const mainCard = document.querySelector('body > .card-container');
 const authorContainer = document.querySelector('.author-container');
 const samuraiContainer = document.querySelector('.samurai-container');
+const eastButton = document.getElementById('nebula-button-east');
 
 toggle.addEventListener('change', () => {
   if (toggle.checked) {
@@ -9,7 +10,8 @@ toggle.addEventListener('change', () => {
     authorContainer.classList.add('slide-down-show');
     authorContainer.setAttribute('aria-hidden', 'false');
     toggle.setAttribute('aria-expanded', 'true');
-    
+    eastButton.style.visibility='hidden';
+
     // Hide samurai when Author section is shown
     samuraiContainer.classList.add('hidden');
   } else {
@@ -17,7 +19,8 @@ toggle.addEventListener('change', () => {
     authorContainer.classList.remove('slide-down-show');
     authorContainer.setAttribute('aria-hidden', 'true');
     toggle.setAttribute('aria-expanded', 'false');
-    
+    eastButton.style.visibility='visible';
+   
     // Show samurai when returning to main view
     samuraiContainer.classList.remove('hidden');
   }
--- assets/js/toggleEast.js
+++ assets/js/toggleEast.js
@@ -3,7 +3,8 @@ document.addEventListener('DOMContentLoaded', function() {
     const eastContainer = document.querySelector('.east-container');
     const mainCard = document.querySelector('.card-container');
     const samuraiContainer = document.querySelector('.samurai-container');
-    
+    const northButton = document.getElementById("nebula-button-north");
+
     let isEastActive = false;
     
     function toggleEast() {
@@ -12,8 +13,10 @@ document.addEventListener('DOMContentLoaded', function() {
             mainCard.classList.add('slide-left-hide');
             eastContainer.classList.add('slide-right-show');
             toggleEastButton.setAttribute('aria-expanded', 'true');
+            northButton.style.visibility = 'hidden';
             isEastActive = true;
             
+
             // Hide samurai when East section is shown
             samuraiContainer.classList.add('hidden');
         } else {
@@ -21,6 +24,7 @@ document.addEventListener('DOMContentLoaded', function() {
             eastContainer.classList.remove('slide-right-show');
             mainCard.classList.remove('slide-left-hide');
             toggleEastButton.setAttribute('aria-expanded', 'false');
+            northButton.style.visibility = 'visible';
             isEastActive = false;
             
             // Show samurai when returning to main view
